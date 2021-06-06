f05caaf0e1c6cafdbf4ba5164675c1d06a1834b5
Samodra <38998978+saamodra@users.noreply.github.com>
2021-06-06 15:46:22 +0700

    Add menu

--- src/main/java/com/example/latihanspring026/controller/view/MenuController.java
+++ src/main/java/com/example/latihanspring026/controller/view/MenuController.java
@@ -1,12 +1,17 @@
 package com.example.latihanspring026.controller.view;
 
+import com.example.latihanspring026.model.Menu;
 import com.example.latihanspring026.service.MenuService;
 import com.example.latihanspring026.service.OrderSegmentService;
 import com.example.latihanspring026.service.OrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MenuController {
@@ -17,12 +22,27 @@ public class MenuController {
     @GetMapping("/menu")
     public String index(Model model) {
         model.addAttribute("menus", menuService.getMenus());
-        return "/menu/index.html";
+        return "/menu/index";
     }
 
     @GetMapping("/menu/tambah")
     public String add(Model model) {
-        return "/menu/add.html";
+        model.addAttribute("menu", new Menu());
+        return "/menu/add";
+    }
+
+    @PostMapping("/menu/tambah")
+    public String store(Menu menu, BindingResult result, Model model) {
+        // set ID menu sesuai dengan id terakhir ditambah 1
+        menu.setIdMenu(menuService.getLastData().getIdMenu() + 1);
+
+        menuService.addMenu(menu);
+
+        if (result.hasErrors()) {
+            return "/menu/add";
+        }
+
+        return "redirect:/menu";
     }
 
 }
